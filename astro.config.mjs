549243347cb8b0330ce1edf43f339c781e23ab87
Tim Wilson <105834883+Tim-The-Wilson@users.noreply.github.com>
2024-11-23 07:35:25 -0800

    fixed mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://tim-the-wilson.github.io",
+	base:"my-repo",
 	integrations: [tailwind()],
 });
